9825ba525a0f180e68b2525905da4b72d55962cd
hata <hata@hata.com>
2013-01-15 10:28:25 +0800

    update

--- backend.js
+++ backend.js
@@ -3,5 +3,5 @@
 {% include vendor/jquery-1.7.1.min.js %}
 {% include vendor/jquery.cookie.js %}
 if ($.cookie("oauth-token")){
-	$("#backend").css("display","block");
+	$("#backend").addClass("show")
 }
