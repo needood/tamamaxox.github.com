bdfbb7569f0f8ec8d91ed7661fad622f44fa4c13
tamamaxox <tamamaspace@gmail.com>
2013-01-16 19:44:11 -0800

    Updated _includes/model.js

--- _includes/model.js
+++ _includes/model.js
@@ -243,7 +243,7 @@ function loadPosts(user, reponame, branch, path, cb) {
     });
   }
   if (window.gotFiles){
-    tree = window.gotFiles
+    tree = window.gotFiles;
     cb(null.getFiles(tree,path,""))
   }else{
     repo.show(function(err, repodata) {
